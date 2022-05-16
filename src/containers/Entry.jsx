982b187b3e327e69cf3cd67936f67a122d78fde7
julianest <julianest12@gmail.com>
2022-05-16 10:56:33 -0500

    [Fix] Se arregla Entry, direccionamiento y estilos

--- src/containers/Entry.jsx
+++ src/containers/Entry.jsx
@@ -1,12 +1,24 @@
 import React from 'react'
-import { Container } from 'react-bootstrap';
+import { Container, Button } from 'react-bootstrap';
 import "../styles/generalStyles.css";
+import { useNavigate } from 'react-router-dom';
 
 const Entry = () => {
+  const navigate = useNavigate()
+
   return (
     <>
-      <Container >
-        <img src="https://res.cloudinary.com/docutv7ug/image/upload/v1652710882/entry_ii3vpu.png" alt="logo" />
+      <Container className="contEntry">
+        <img src="https://res.cloudinary.com/docutv7ug/image/upload/v1652710882/entry_ii3vpu.png" alt="logo" className="imgEntry" />
+        <div className="continitgen" >
+          <h1 className="contenInit">
+            STYLE YOUR CLOTHES
+          </h1>
+          <div className="contBotonesEnt">
+            <Button className="buttonsEntry" variant="outline-light" onClick={() => navigate("/login")} >Sign In</Button>
+            <Button className="buttonsEntry" variant="outline-secondary" onClick={() => navigate("/register")}>Sign Up</Button>
+          </div>
+        </div>
       </Container>
     </>
   )
--- src/redux/actions/actionProduct.jsx
+++ src/redux/actions/actionProduct.jsx
@@ -1,53 +1,53 @@
-import {addDoc, collection, getDocs, query } from "firebase/firestore"
+import { addDoc, collection, getDocs, query } from "firebase/firestore"
 import { DB } from "../../firebase/FirebaseConfig"
 import { typesProduct } from "../types/typesProduct"
 
 export const addProductAsync = (product) => {
-    return (dispath) => {
-           
-        addDoc(collection(DB, "ProductsDB"), product)
-            .then(resp => {
-                dispath(addProductSync(product))                
+  return (dispath) => {
 
-            })
-            .catch(error => {
-                console.warn(error)
-            })
+    addDoc(collection(DB, "ProductsDB"), product)
+      .then(resp => {
+        dispath(addProductSync(product))
 
-    }
+      })
+      .catch(error => {
+        console.warn(error)
+      })
+
+  }
 }
 
 
 export const addProductSync = (product) => {
-    return {
-        type: typesProduct.add,
-        payload: product
-    }
+  return {
+    type: typesProduct.add,
+    payload: product
+  }
 }
 
 
 
 //---- listar peliculas---//
 export const listProductAsync = () => {
-	return async (dispath) => {
-		const collectionListar = await getDocs(collection(DB, "ProductsDB"))
-		console.log(collectionListar)
-		const querySearch = query(collection(DB, "ProductsDB"))
-		const queryListar = await getDocs(querySearch)
-		console.log(queryListar)
-		const products = []
-		collectionListar.forEach(lista => {
-			products.push({
-				...lista.data()
-			})
-		})
-		dispath(listProductSync(products))
-	}
+  return async (dispath) => {
+    const collectionListar = await getDocs(collection(DB, "ProductsDB"))
+    console.log(collectionListar)
+    const querySearch = query(collection(DB, "ProductsDB"))
+    const queryListar = await getDocs(querySearch)
+    console.log(queryListar)
+    const products = []
+    collectionListar.forEach(lista => {
+      products.push({
+        ...lista.data()
+      })
+    })
+    dispath(listProductSync(products))
+  }
 }
 
 export const listProductSync = (product) => {
-	return {
-		type: typesProduct.list,
-		payload: product
-	}
+  return {
+    type: typesProduct.list,
+    payload: product
+  }
 }
